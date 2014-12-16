634d38601d171872a2878a7c52afd55e79f89c29
Daniel Boline <ddboline@gmail.com>
2014-12-16 15:48:21 -0500

    ...

--- average_word_vectors.py
+++ average_word_vectors.py
@@ -6,13 +6,14 @@ from sklearn.ensemble import RandomForestClassifier
 from KaggleWord2VecUtility import KaggleWord2VecUtility
 import pandas as pd
 import numpy as np
+import itertools
 
 def average_vectors():
     labeledtrain_data = pd.read_csv('labeledTrainData.tsv', header=0, delimiter='\t', quoting=3)
     unlabeledtrain_data = pd.read_csv('unlabeledTrainData.tsv', header=0, delimiter='\t', quoting=3)
     test_data = pd.read_csv('testData.tsv', header=0, delimiter='\t', quoting=3)
 
-    print len(chain(labeledtrain_data, unlabeledtrain_data, test_data))
+    print len(itertools.chain(labeledtrain_data, unlabeledtrain_data, test_data))
 
 
 if __name__ == '__main__':
