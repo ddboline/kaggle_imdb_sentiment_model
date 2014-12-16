b79e47887ed020dda9be65c199a04fb27a14200a
Daniel Boline <ddboline@gmail.com>
2014-12-16 15:46:36 -0500

    ...

--- KaggleWord2VecUtility.py
+++ KaggleWord2VecUtility.py
@@ -14,7 +14,7 @@ class KaggleWord2VecUtility(object):
     """KaggleWord2VecUtility is a utility class for processing raw HTML text into segments for further learning"""
 
     @staticmethod
-    def review_to_wordlist( review, remove_stopwords=False ):
+    def review_to_wordlist(review, remove_stopwords=False):
         # Function to convert a document to a sequence of words,
         # optionally removing stop words.  Returns a list of words.
         #
@@ -37,7 +37,7 @@ class KaggleWord2VecUtility(object):
 
     # Define a function to split a review into parsed sentences
     @staticmethod
-    def review_to_sentences( review, tokenizer, remove_stopwords=False ):
+    def review_to_sentences(review, tokenizer, remove_stopwords=False):
         # Function to split a review into parsed sentences. Returns a
         # list of sentences, where each sentence is a list of words
         #
@@ -50,8 +50,8 @@ class KaggleWord2VecUtility(object):
             # If a sentence is empty, skip it
             if len(raw_sentence) > 0:
                 # Otherwise, call review_to_wordlist to get a list of words
-                sentences.append( KaggleWord2VecUtility.review_to_wordlist( raw_sentence, \
-                  remove_stopwords ))
+                sentences.append(KaggleWord2VecUtility.review_to_wordlist(raw_sentence, \
+                  remove_stopwords))
         #
         # Return the list of sentences (each sentence is a list of words,
         # so this returns a list of lists
--- bag_of_words_model.py
+++ bag_of_words_model.py
@@ -18,7 +18,7 @@ def bag_of_words_model():
     labeledtrain_data = pd.read_csv('labeledTrainData.tsv', header=0, delimiter='\t', quoting=3)
     #unlabeledtrain_data = pd.read_csv('unlabeledTrainData.tsv', header=0, delimiter='\t', quoting=3)
 
-    clean_labeledtrain_reviews = map( clean_review_function, labeledtrain_data['review'] )
+    clean_labeledtrain_reviews = map(clean_review_function, labeledtrain_data['review'])
 
 
     vectorizer = CountVectorizer(analyzer = 'word', tokenizer = None,  preprocessor = None, stop_words = None, max_features = 1000)
@@ -28,38 +28,38 @@ def bag_of_words_model():
     train_review_subset_y = labeledtrain_data['sentiment'][::2]
     test_review_subset_x = clean_labeledtrain_reviews[1::2]
     test_review_subset_y = labeledtrain_data['sentiment'][1::2]
-    
+
     train_data_features = vectorizer.fit_transform(train_review_subset_x).toarray()
 
-    
+
     forest = RandomForestClassifier(n_estimators = 100)
-    forest = forest.fit( train_data_features, train_review_subset_y )
-    
+    forest = forest.fit(train_data_features, train_review_subset_y)
+
     test_data_features = vectorizer.transform(test_review_subset_x).toarray()
 
-    
-    print forest.score( test_data_features , test_review_subset_y )
-    
+
+    print forest.score(test_data_features, test_review_subset_y)
+
     del train_review_subset_x, train_review_subset_y, test_review_subset_x, test_review_subset_y, test_data_features, train_data_features
 
-    
+
     train_data_features = vectorizer.fit_transform(clean_labeledtrain_reviews).toarray()
 
-    
-    forest = forest.fit( train_data_features, labeledtrain_data['sentiment'] )
 
-    
+    forest = forest.fit(train_data_features, labeledtrain_data['sentiment'])
+
+
     test_data = pd.read_csv('testData.tsv', header=0, delimiter='\t', quoting=3)
 
-    
-    clean_test_reviews = map( clean_review_function , test_data['review'] )
+
+    clean_test_reviews = map(clean_review_function, test_data['review'])
 
 
     test_data_features = vectorizer.transform(clean_test_reviews).toarray()
 
 
     result = forest.predict(test_data_features)
-    
+
     output = pd.DataFrame(data={'id': test_data['id'], 'sentiment': result})
     output.to_csv('bag_of_words_model.csv', index=False, quoting=3)
 
