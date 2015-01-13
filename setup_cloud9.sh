8b22ef909553ba4660731b4831ce186c40de510a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-01-13 17:04:47 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -3,5 +3,10 @@
 sudo apt-get update
 sudo apt-get install -y python-sklearn python-pandas python-pip
 sudo pip install nltk memory_profiler gensim
-# python -m nltk
+if [[ $- != *i* ]]; then
+    # Shell is non-interactive.  Be done now
+    return
+fi
+
+python -m nltk "nltk.download()"
 # python -c "import nltk; nltk.download()"
