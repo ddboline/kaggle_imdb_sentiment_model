8164eff03c078abcdc3319f19d86ce13431daa7e
Daniel Boline <ddboline@gmail.com>
2015-01-13 19:30:13 -0500

    ...

--- run_aws.sh
+++ run_aws.sh
@@ -3,6 +3,6 @@
 DATE=`date +%Y%m%d%H%M%S`
 N=1000
 git pull
-python my_model.py $N > temp.out 2> temp.err
+time python my_model.py $N > temp.out 2> temp.err
 tar zcvf temp_output_${N}_${DATE}.tar.gz temp.out temp.err *.csv
 python save_to_s3.py temp_output_${N}_${DATE}.tar.gz
