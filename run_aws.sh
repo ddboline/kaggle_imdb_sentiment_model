b1d0b8fc6dd3bc517feb6dd1db244f5d686ce63a
Daniel Boline <ddboline@gmail.com>
2015-01-13 19:28:02 -0500

    ...

--- run_aws.sh
+++ run_aws.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DATE=`date +%Y%m%d%H%M%S`
+N=100
 git pull
-python my_model.py 100 > temp.out 2> temp.err
-tar zcvf temp_output_${DATE}.tar.gz temp.out temp.err *.csv
-scp temp_output_${DATE}.tar.gz ddboline@ddbolineathome.mooo.com:~/setup_files/build/kaggle_imdb_sentiment_model/
+python my_model.py $N > temp.out 2> temp.err
+tar zcvf temp_output_${N}_${DATE}.tar.gz temp.out temp.err *.csv
+python save_to_s3.py temp_output_${N}_${DATE}.tar.gz
