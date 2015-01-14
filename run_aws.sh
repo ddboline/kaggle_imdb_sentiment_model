8cdaa140d67c829690f0a424b7e38a1789da9c39
Daniel Boline <ddboline@gmail.com>
2015-01-14 17:46:59 -0500

    ...

--- run_aws.sh
+++ run_aws.sh
@@ -6,7 +6,6 @@ if [ -z $1 ]; then
 else
     N=$1
 fi
-git pull
 time python my_model.py $N > temp.out 2> temp.err
 tar zcvf temp_output_${N}_${DATE}.tar.gz temp.out temp.err *.csv
 python save_to_s3.py temp_output_${N}_${DATE}.tar.gz
