e89df1cad910f5c3a187e07f0612d3e7b1419770
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-27 19:51:36 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-DATE=`date +%Y%m%d%H%M%S`
-if [ -z $1 ]; then
-    N=1000
-else
-    N=$1
-fi
+time ./load_data.py
 
-time python my_model.py # $N > temp.out 2> temp.err
+# time python my_model.py # $N > temp.out 2> temp.err
 
-ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk imdb_sentiment_model_DONE"
+# ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk imdb_sentiment_model_DONE"
