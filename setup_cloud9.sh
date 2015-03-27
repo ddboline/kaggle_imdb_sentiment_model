6848387b6c7170b35403df83c0738d319748f82a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-27 14:27:38 -0400

    update to use pydeb made packages instead of pip

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python-boto python-sklearn python-pandas python-pip python-virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install nltk memory_profiler gensim
-if [[ $- != *i* ]]; then
-    # Shell is non-interactive.  Be done now
-    return
-fi
+sudo apt-get install -y python-boto python-sklearn python-pandas 
+
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo apt-get update
+
+sudo apt-get install -y --force-yes python-nltk python-gensim
+
+scp ddboline@ddbolineathome.mooo.com:~/nltk_data_full.tar.gz .
+tar zxvf nltk_data_full.tar.gz
+rm nltk_data_full.tar.gz
+
+# if [[ $- != *i* ]]; then
+#     # Shell is non-interactive.  Be done now
+#     return
+# fi
 
 # python -m nltk -c "nltk.download()"
-python -c "import nltk; nltk.download()"
+# python -c "import nltk; nltk.download()"
