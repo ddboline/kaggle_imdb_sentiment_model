f9f98061c7988951ee933ea06d30a119a50463a3
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-28 09:01:28 -0400

    ...

--- load_data.py
+++ load_data.py
@@ -11,24 +11,22 @@ import numpy as np
 
 from sklearn.feature_extraction.text import CountVectorizer
 
-from generate_biased_word_list import generate_biased_word_list
 from KaggleWord2VecUtility import review_to_wordlist, review_to_sentences
 
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
 def clean_review(review):
-    list_of_words = review_to_wordlist(review, remove_stopwords=False)
-    return ' '.join(list_of_words)
+    list_of_sentences = review_to_sentences(review, tokenizer, remove_stopwords=False)
+    return list_of_sentences
 
 def load_data_bagofwords(do_plots=False):
     traindf = pd.read_csv('labeledTrainData.tsv.gz', compression='gzip', delimiter='\t', header=0, quoting=3)
     testdf = pd.read_csv('testData.tsv.gz', compression='gzip', delimiter='\t', header=0, quoting=3)
-    #unlabeled_traindf = pd.read_csv('unlabeledTrainData.tsv.gz', compression='gzip', delimiter='\t', header=0, quoting=3)
-    word_count_df = pd.read_csv('word_count.csv.gz', compression='gzip')
-    
-    cond0 = word_count_df['count']>500
-    cond1 = word_count_df['frac']>0.1
-    biased_word_list = {w: n for n, w in enumerate(list(word_count_df[cond0 & cond1]['word']))}
+    unlabeled_traindf = pd.read_csv('unlabeledTrainData.tsv.gz', compression='gzip', delimiter='\t', header=0, quoting=3)
     
     traincleanreview = traindf['review'].apply(clean_review)
+    print type(traincleanreview)
+    exit(0)
     testcleanreview = testdf['review'].apply(clean_review)
     vectorizer = CountVectorizer(analyzer='word', vocabulary=biased_word_list)
     trainwvector = vectorizer.transform(traincleanreview).toarray()
