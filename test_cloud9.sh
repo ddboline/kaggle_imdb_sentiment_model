efdc6be12612911828bbb90a9320623212c69684
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-28 09:36:53 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -4,4 +4,4 @@
 
 # time python my_model.py # $N > temp.out 2> temp.err
 
-# ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk imdb_sentiment_model_DONE"
+ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk imdb_sentiment_model_DONE"
